1117ca12791573f495a9241234f50f10b21076ad
keflavich <keflavich@7a03b35a-38c0-11de-8fef-1f5ae417a2df>
2009-11-07 20:05:40 +0000

    Added powerfit, a simple power-law fitter (unlike plfit, which is a power-law distribution fitter)
    git-svn-id: https://agpy.googlecode.com/svn/trunk@80 7a03b35a-38c0-11de-8fef-1f5ae417a2df

---
+++ powerfit.py
@@ -0,0 +1,19 @@
+import mpfit
+
+def powerfit(xax,data,alphaguess=-2.0,scaleguess=1.0,quiet=True):
+    """
+    Fit a power law (a line in log-space) to data as a function of x
+    differs from 'plfit' because plfit fits a power law distribution, 
+    this code simply fits a power law
+    """
+    
+    logdata = log10(data)
+
+    def mpfitfun(data):
+        def f(p,fjac=None): return [0,np.ravel((log10(p[0])+log10(xax)*p[1])-data)]
+        return f
+        
+    mp = mpfit.mpfit(mpfitfun(logdata),xall=[scaleguess,alphaguess],quiet=quiet)
+    fitp = mp.params
+
+    return fitp,mp
