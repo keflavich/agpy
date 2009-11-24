b8ef5903ed79df8e26dc92f969dc4a430b1aec76
keflavich <keflavich@7a03b35a-38c0-11de-8fef-1f5ae417a2df>
2009-11-24 19:54:45 +0000

    Made agpy installable
    git-svn-id: https://agpy.googlecode.com/svn/trunk@83 7a03b35a-38c0-11de-8fef-1f5ae417a2df

--- plfit/setup.py
+++ plfit/setup.py
@@ -2,22 +2,31 @@
 #from distutils.extension import Extension
 from numpy.distutils.core import setup
 from numpy.distutils.core import Extension
+#from numpy.distutils.core import build_ext
 from Cython.Distutils import build_ext
 import numpy
 
 print "To create cplfit.so (for importing), call command: "
 print "python setup.py build_ext --inplace"
 
-ext_cplfit = Extension("cplfit", ["cplfit.pyx"], include_dirs = [numpy.get_include(),
+try:
+    from numpy.distutils.misc_util import get_numpy_include_dirs
+    numpy_include_dirs = get_numpy_include_dirs()
+except AttributeError:
+    numpy_include_dirs = numpy.get_include()
+
+
+ext_cplfit = Extension("cplfit", ["cplfit.pyx"], include_dirs = [numpy_include_dirs,
    '/Library/Frameworks/Python.framework/Versions/2.6/lib/python2.6/site-packages/Cython/','.'],
    extra_compile_args=['-O3'])
-ext_fplfit = Extension(name="fplfit",
-                    sources=["fplfit.f"])
+#ext_fplfit = Extension(name="fplfit",
+#                    sources=["fplfit.f"])
 
-setup(
-    ext_modules = [ ext_cplfit ],
-    cmdclass = {'build_ext': build_ext}
-)
+if __name__=="main":
+    setup(
+        ext_modules = [ ext_cplfit ],
+        cmdclass = {'build_ext': build_ext}
+    )
 
 print "I can't get numpy.distutils to compile the fortran.  To do it yourself, run some variant of:"
 print 'f2py -c fplfit.f -m fplfit'
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='agpy',
+      version='0.1',
+      description='AGPY, Adam Ginsburg\'s Python Code',
+      author='Adam Ginsburg',
+      author_email='adam.ginsburg@colorado.edu',
+      url='http://code.google.com/p/agpy/',
+      packages=['.','plfit'],
+     )
