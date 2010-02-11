argh - fixed the last error.  Normalization issues and then some
git-svn-id: https://agpy.googlecode.com/svn/trunk@123 7a03b35a-38c0-11de-8fef-1f5ae417a2df

agpy/psds.py
@@ -12,9 +12,9 @@ def PSD2(image,oned=True,return_index=True,wavenumber=False):
 
     image[image!=image] = 0
     #acorr = scipy.stsci.convolve.correlate2d(image,image,fft=True,mode='constant')
-    #acorr = correlate2d(image,image)
-    #psd2 = numpy.abs( numpy.fft.fftshift( numpy.fft.fft2(acorr) ) )
-    psd2 = numpy.abs( correlate2d(image,image,psd=True) ) 
+    acorr = correlate2d(image,image)
+    psd2 = numpy.abs( numpy.fft.fftshift( numpy.fft.fft2(acorr) ) )
+    #psd2 = numpy.abs( ( correlate2d(image,image,psd=True) ) )
 
     xx,yy = numpy.indices(image.shape)
     rr = numpy.sqrt((xx-image.shape[0] / 2)**2+(yy-image.shape[1] / 2)**2)