f5f55f5afc4f3b3ec945f8823c1ef4bb30bb58a1
keflavich <keflavich@7a03b35a-38c0-11de-8fef-1f5ae417a2df>
2010-02-11 22:46:30 +0000

    updated psds.py to be more compatible with the package without requiring a complete install.  Also fixed up units
    git-svn-id: https://agpy.googlecode.com/svn/trunk@121 7a03b35a-38c0-11de-8fef-1f5ae417a2df

--- agpy/psds.py
+++ agpy/psds.py
@@ -1,18 +1,20 @@
 import numpy
-from agpy import correlate2d
+from correlate2d import correlate2d
 
-def PSD2(image,oned=True,return_index=True):
+def PSD2(image,oned=True,return_index=True,wavenumber=False):
     """
     Two-dimensional PSD
     oned - return radial profile of 2D PSD (i.e. mean power as a function of spatial frequency)
+           plot(freq,zz) is a power spectrum
     return_index - if true, the first return item will be the indexes
+    wavenumber - if one dimensional and return_index set, will return a normalized wavenumber instead
     """
 
     image[image!=image] = 0
     #acorr = scipy.stsci.convolve.correlate2d(image,image,fft=True,mode='constant')
     #acorr = correlate2d(image,image)
     #psd2 = numpy.abs( numpy.fft.fftshift( numpy.fft.fft2(acorr) ) )
-    psd2 = numpy.abs( correlate2d(image,image,psd=True) )
+    psd2 = numpy.abs( numpy.fft.fftshift( correlate2d(image,image,psd=True) ) )
 
     xx,yy = numpy.indices(image.shape)
     rr = numpy.sqrt((xx-image.shape[0] / 2)**2+(yy-image.shape[1] / 2)**2)
@@ -23,7 +25,10 @@ def PSD2(image,oned=True,return_index=True):
         zz = numpy.array([ psd2[xx[rr.round()==ii],yy[rr.round()==ii]].mean() for ii in freq])
 
         if return_index:
-            return freq/len(freq),zz
+            if wavenumber:
+                return len(freq)/freq,zz
+            else:
+                return freq/len(freq),zz
         else:
             return zz
     else:
