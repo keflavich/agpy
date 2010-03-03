9b4e72c3fc6866559bd2db4eda89ffed7efe5520
keflavich <keflavich@7a03b35a-38c0-11de-8fef-1f5ae417a2df>
2010-03-03 21:52:57 +0000

    Committed the much-simplified correlate2d and added test code for it (using gaussians)
    git-svn-id: https://agpy.googlecode.com/svn/trunk@147 7a03b35a-38c0-11de-8fef-1f5ae417a2df

--- agpy/correlate2d.py
+++ agpy/correlate2d.py
@@ -1,4 +1,5 @@
 import numpy
+import convolve
 
 try:
     #print "Attempting to import scipy.  If you experience a bus error at this step, it is likely because of a bad scipy install"
@@ -11,65 +12,23 @@ except ImportError:
     ifft2 = numpy.fft.ifft2
 
 
-def correlate2d(im1,im2,psd=False,psdshift=True,crop=True,pad=True):
+def correlate2d(im1,im2,**kwargs):
     """
     Cross-correlation of two images of arbitrary size.  Returns an image
-    cropped to the size of the first input image
+    cropped to the largest of each dimension of the input images
 
     Options:
-    psd - if true, return fft(im1)*fft(im2[::-1,::-1]), which is the power
+    return_fft - if true, return fft(im1)*fft(im2[::-1,::-1]), which is the power
         spectral density
-    psdshift - if true, return the shifted psd so that the DC component is in
+    fftshift - if true, return the shifted psd so that the DC component is in
         the center of the image
+    pad - Default on.  Zero-pad image to the nearest 2^n
+    crop - Default on.  Return an image of the size of the largest input image.
+        If the images are asymmetric in opposite directions, will return the largest 
+        image in both directions.
 
     WARNING: Normalization may be arbitrary if you use the PSD
-
-    Most of this code is duplicated from convolve.py, but I don't simply call
-    convolve(im1,im2[::-1,::-1]) because the PSD that is created in an internal
-    step is useful.
     """
 
-    # NAN catching
-    im1[im1!=im1] = 0
-    im2[im2!=im2] = 0
-
-    shape1 = im1.shape
-    shape2 = im2.shape
-    if pad:
-        # find ideal size (power of 2) for fft.  Can add shapes because they are tuples
-        fsize = 2**numpy.ceil(numpy.log2(numpy.max(shape1+shape2)))
-        newshape = numpy.array([fsize,fsize])
-    else:
-        newshape = numpy.array([numpy.max([shape1[0],shape2[0]]),numpy.max([shape1[1],shape2[1]])]) #numpy.array(shape1)+numpy.array(shape2)
-    centerx, centery = newshape/2.
-    im1quarter1x, im1quarter1y = centerx - shape1[0]/2.,centery - shape1[1]/2.
-    im1quarter3x, im1quarter3y = centerx + shape1[0]/2.,centery + shape1[1]/2.
-    im2quarter1x, im2quarter1y = centerx - shape2[0]/2.,centery - shape2[1]/2.
-    im2quarter3x, im2quarter3y = centerx + shape2[0]/2.,centery + shape2[1]/2.
-    bigim1 = numpy.zeros(newshape,dtype=numpy.float64)
-    bigim2 = numpy.zeros(newshape,dtype=numpy.float64)
-    bigim1[im1quarter1x:im1quarter3x,im1quarter1y:im1quarter3y] = im1
-    bigim2[im2quarter1x:im2quarter3x,im2quarter1y:im2quarter3y] = im2[::-1,::-1]
-    imfft1 = fft2(bigim1)
-    imfft2 = fft2(bigim2)
-    fftmult = imfft1*imfft2
-
-    quarter1x = numpy.min([im1quarter1x,im2quarter1x])
-    quarter3x = numpy.max([im1quarter3x,im2quarter3x])
-    quarter1y = numpy.min([im1quarter1y,im2quarter1y])
-    quarter3y = numpy.max([im1quarter3y,im2quarter3y])
-    if psd:
-        if psdshift: # default on
-            shiftfftmult = numpy.fft.fftshift(fftmult)
-            result = shiftfftmult[ quarter1x:quarter3x, quarter1y:quarter3y ] 
-            return result
-        else:
-            print "Warning: no fft shift - not cropped!"
-            return fftmult
-    else:
-        rifft = numpy.fft.fftshift(ifft2( fftmult ))
-        if crop: # default on
-            return rifft[ quarter1x:quarter3x, quarter1y:quarter3y ]
-        else: 
-            return rifft
+    return convolve.convolve(im1,im2[::-1,::-1],**kwargs)
 
---
+++ tests/test_correlate2d.py
@@ -0,0 +1,78 @@
+from pylab import *
+from agpy import correlate2d,psds,convolve
+
+xx,yy = indices([2**7*1.5,2**7*1.5])
+rr1 = sqrt((xx-2**7)**2+(yy-2**7)**2)
+rr2 = sqrt((xx-2**6*1.5)**2+(yy-2**6*1.5)**2)
+
+ee1 = exp(-rr1**2/2.0**4)
+ee2 = exp(-rr2**2/2.0**4)
+
+acorr1pc = correlate2d(ee1,ee1,pad=True)
+acorr1nc = correlate2d(ee1,ee1,pad=False)
+acorr2pc = correlate2d(ee2,ee2,pad=True)
+acorr2nc = correlate2d(ee2,ee2,pad=False)
+
+acorr1pn = correlate2d(ee1,ee1,pad=True,crop=False)
+acorr1nn = correlate2d(ee1,ee1,pad=False,crop=False)
+acorr2pn = correlate2d(ee2,ee2,pad=True,crop=False)
+acorr2nn = correlate2d(ee2,ee2,pad=False,crop=False)
+
+aconv1n = convolve(ee1,ee1,pad=False)
+aconv2n = convolve(ee2,ee2,pad=False)
+aconv1p = convolve(ee1,ee1,pad=True)
+aconv2p = convolve(ee2,ee2,pad=True)
+
+xcorr1pc = correlate2d(ee1,ee2,pad=True)
+xcorr1nc = correlate2d(ee1,ee2,pad=False)
+xcorr2pc = correlate2d(ee2,ee1,pad=True)
+xcorr2nc = correlate2d(ee2,ee1,pad=False)
+
+xcorr1pn = correlate2d(ee1,ee2,pad=True,crop=False)
+xcorr1nn = correlate2d(ee1,ee2,pad=False,crop=False)
+xcorr2pn = correlate2d(ee2,ee1,pad=True,crop=False)
+xcorr2nn = correlate2d(ee2,ee1,pad=False,crop=False)
+
+figure(1)
+clf()
+subplot(221); imshow(abs(acorr1pc)); title("acorr1 pad"); colorbar()
+subplot(222); imshow(abs(acorr1nc)); title("acorr1 no pad"); colorbar()
+subplot(223); imshow(abs(acorr2pc)); title("acorr2 pad"); colorbar()
+subplot(224); imshow(abs(acorr2nc)); title("acorr2 no pad"); colorbar()
+
+figure(4)
+clf()
+subplot(221); imshow(abs(acorr1pn)); title("acorr1 no crop pad"); colorbar()
+subplot(222); imshow(abs(acorr1nn)); title("acorr1 no crop no pad"); colorbar()
+subplot(223); imshow(abs(acorr2pn)); title("acorr2 no crop pad"); colorbar()
+subplot(224); imshow(abs(acorr2nn)); title("acorr2 no crop no pad"); colorbar()
+
+figure(2)
+clf()
+subplot(221); imshow(abs(ee1)); title("ee1"); colorbar()
+subplot(222); imshow(aconv1p); title("aconv1"); colorbar()
+subplot(223); imshow(abs(ee2)); title("ee2"); colorbar()
+subplot(224); imshow(aconv2p); title("aconv2"); colorbar()
+
+figure(3)
+clf()
+subplot(221); imshow(aconv1p); title("aconv1 pad"); colorbar()
+subplot(222); imshow(aconv1n); title("aconv1 no pad"); colorbar()
+subplot(223); imshow(aconv2p); title("aconv2 pad"); colorbar()
+subplot(224); imshow(aconv2n); title("aconv2 no pad"); colorbar()
+
+
+figure(5)
+clf()
+subplot(221); imshow(abs(xcorr1pc)); title("xcorr1 pad"); colorbar()
+subplot(222); imshow(abs(xcorr1nc)); title("xcorr1 no pad"); colorbar()
+subplot(223); imshow(abs(xcorr2pc)); title("xcorr2 pad"); colorbar()
+subplot(224); imshow(abs(xcorr2nc)); title("xcorr2 no pad"); colorbar()
+
+figure(6)
+clf()
+subplot(221); imshow(abs(xcorr1pn)); title("xcorr1 no crop pad"); colorbar()
+subplot(222); imshow(abs(xcorr1nn)); title("xcorr1 no crop no pad"); colorbar()
+subplot(223); imshow(abs(xcorr2pn)); title("xcorr2 no crop pad"); colorbar()
+subplot(224); imshow(abs(xcorr2nn)); title("xcorr2 no crop no pad"); colorbar()
+
