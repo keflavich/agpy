682e99d3bad762700058c28070c6df283482c3d9
keflavich <keflavich@7a03b35a-38c0-11de-8fef-1f5ae417a2df>
2010-06-17 18:27:18 +0000

    azimuthalAverage updated to return the correct X-axis values now (previous could have size mismatch) montage documentation updated
    git-svn-id: https://agpy.googlecode.com/svn/trunk@184 7a03b35a-38c0-11de-8fef-1f5ae417a2df

--- agpy/radialprofile.py
+++ agpy/radialprofile.py
@@ -1,13 +1,16 @@
 import numpy as np
 
-def azimuthalAverage(image, center=None, stddev=False, returnradii=False ):
+def azimuthalAverage(image, center=None, stddev=False, returnradii=False, return_nr=False ):
     """
     Calculate the azimuthally averaged radial profile.
 
     image - The 2D image
     center - The [x,y] pixel coordinates used as the center. The default is 
              None, which then uses the center of the image (including 
-             fracitonal pixels).
+             fractional pixels).
+    stddev - if specified, return the azimuthal standard deviation instead of the average
+    returnradii - if specified, return (radii_array,radial_profile)
+    return_nr   - if specified, return number of pixels per radius *and* radius
     
     Taken from http://www.astrobetter.com/wiki/tiki-index.php?page=python_radial_profiles
     via Jessica R. Lu
@@ -15,7 +18,7 @@ def azimuthalAverage(image, center=None, stddev=False, returnradii=False ):
     # Calculate the indices from the image
     y, x = np.indices(image.shape)
 
-    if not center:
+    if center is None:
         center = np.array([(x.max()-x.min())/2.0, (y.max()-y.min())/2.0])
 
     r = np.hypot(x - center[0], y - center[1])
@@ -34,13 +37,13 @@ def azimuthalAverage(image, center=None, stddev=False, returnradii=False ):
     rind = np.where(deltar)[0] + 1   # location of changed radius (minimum 1 point)
     nr = np.concatenate([rind[0:1], rind[1:] - rind[:-1]]) # number of radius bin
                                      # concatenate to include center pixel / innermost bin
-    
+
     # Cumulative sum to figure out sums for each radius bin
     csim = np.cumsum(i_sorted, dtype=float)
     tbin = np.concatenate([csim[0:1], csim[rind[1:]] - csim[rind[:-1]]]) # include innermost bin
 
     radial_prof = tbin / nr
-    
+
     if stddev:
         # find azimuthal standard deviation
         r_int[r_int==r_int.max()] = r_int.max() - 1  # set last bin equal to second-to-last bin
@@ -49,12 +52,16 @@ def azimuthalAverage(image, center=None, stddev=False, returnradii=False ):
         rad_std = np.sqrt( ( np.concatenate([rad_diffsum[0:1], rad_diffsum[rind[1:]] - rad_diffsum[rind[:-1]]]) ) / nr )
         
         if returnradii: 
-            return np.unique(r_int),rad_std
+            return r_int[rind],rad_std
+        elif return_nr:
+            return nr,r_int[rind],rad_std
         else:
             return rad_std
 
     else:
         if returnradii: 
-            return np.unique(r_int),radial_prof
+            return r_int[rind],radial_prof
+        elif return_nr:
+            return nr,r_int[rind],radial_prof
         else:
             return radial_prof
