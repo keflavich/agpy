18be1589f6c62517662d12703d8ee54519aa986c
keflavich <keflavich@7a03b35a-38c0-11de-8fef-1f5ae417a2df>
2010-11-18 06:28:24 +0000

    fixed ratosexagesimal code
    git-svn-id: https://agpy.googlecode.com/svn/trunk@268 7a03b35a-38c0-11de-8fef-1f5ae417a2df

--- agpy/ratosexagesimal.py
+++ agpy/ratosexagesimal.py
@@ -3,15 +3,19 @@ from numpy import sign
 def ratos(ra):
     r = ra/15.
     deg = r - (r % 1)
+    deg -= (deg % 1)
     min = (r % 1) * 60.
     sec = (min % 1) * 60
-    rs = "%02.0f:%02.0f:%05.2f" % (deg,min,sec)
+    min -= (min % 1)
+    rs = "%02.0f:%02.0f:%05.3f" % (deg,min,sec)
     return rs
 
 def dectos(dec):
     deg = sign(dec) * (abs(dec) - (abs(dec) % 1))
+    deg -= (deg%1)
     min = (abs(dec) % 1) * 60.
     sec = (min % 1) * 60
-    decs = "%+02.0f:%02.0f:%05.2f" % (deg,min,sec)
+    min -= (min % 1)
+    decs = "%+02.0f:%02.0f:%05.3f" % (deg,min,sec)
     return decs
 
