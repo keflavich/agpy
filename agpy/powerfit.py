c6ee020ad701f75e4832a2b857f364d3631d2d78
keflavich <keflavich@7a03b35a-38c0-11de-8fef-1f5ae417a2df>
2010-11-22 22:19:46 +0000

    added errors to powerfit (so that mp.perror works out right)
    git-svn-id: https://agpy.googlecode.com/svn/trunk@270 7a03b35a-38c0-11de-8fef-1f5ae417a2df

--- agpy/powerfit.py
+++ agpy/powerfit.py
@@ -1,7 +1,7 @@
 import mpfit
 import numpy as np
 
-def powerfit(xax,data,alphaguess=-2.0,scaleguess=1.0,quiet=True):
+def powerfit(xax,data,err=None,alphaguess=-2.0,scaleguess=1.0,quiet=True):
     """
     Fit a power law (a line in log-space) to data as a function of x
     differs from 'plfit' because plfit fits a power law distribution, 
@@ -9,12 +9,13 @@ def powerfit(xax,data,alphaguess=-2.0,scaleguess=1.0,quiet=True):
     """
     
     logdata = np.log10(data)
+    if err is None: err = np.ones(data.shape,dtype='float')
 
-    def mpfitfun(data):
-        def f(p,fjac=None): return [0,np.ravel((np.log10(p[0])+np.log10(xax)*p[1])-data)]
+    def mpfitfun(data,err):
+        def f(p,fjac=None): return [0,np.ravel(((np.log10(p[0])+np.log10(xax)*p[1])-data)/err)]
         return f
         
-    mp = mpfit.mpfit(mpfitfun(logdata),xall=[scaleguess,alphaguess],quiet=quiet)
+    mp = mpfit.mpfit(mpfitfun(logdata,err),xall=[scaleguess,alphaguess],quiet=quiet)
     fitp = mp.params
 
     return fitp,mp
