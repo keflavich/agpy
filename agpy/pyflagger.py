Fixed error in azimuthal average that led to non-centered azimuthal averages having erroneous first values.  Added bin-size feature.
Added some new PCA tool features for use with pyflagger

Updated plfit plotting procedures



git-svn-id: https://agpy.googlecode.com/svn/trunk@316 7a03b35a-38c0-11de-8fef-1f5ae417a2df

agpy/pyflagger.py
@@ -210,6 +210,7 @@ class Flagger:
       self.whempty = concatenate([arange(i+j,i+self.scanlen) for i,j in zip(self.scanstarts,self.scanlengths) ]).ravel()
       self.whscan[self.whempty] = 0
 
+      self.tsshape = [self.nscans*self.scanlen,self.ngoodbolos]
       self.datashape = [self.nscans,self.scanlen,self.ngoodbolos]
 
       if self.needed_once_struct is not None:
@@ -224,7 +225,9 @@ class Flagger:
       self.atmo_one    = self.bgps['atmo_one'][0][self.whscan,:].astype('float')
       self.noise       = self.bgps['noise'][0][self.whscan,:].astype('float')
       self.scalearr    = self.bgps['scalearr'][0][self.whscan,:].astype('float')
+      self.scale_coeffs = self.bgps['scale_coeffs'][0].astype('float')
       self.weight      = self.bgps['weight'][0][self.whscan,:].astype('float')
+      self.weight_by_bolo = self.weight.mean(axis=0)
       self.zeromedian  = self.astrosignal * 0
       self.flags       = self.bgps['flags'][0][self.whscan,:]
       self.flags.shape = self.datashape
@@ -294,6 +297,10 @@ class Flagger:
       'mapped_timestream': lambda: self.mapped_timestream,
       'itermedian': lambda: itermedian(self.ac_bolos * self.scalearr),
       'zeromedian': lambda: self.atmo_one,
+      'PCA_acb':     lambda: reshape(efuncs(reshape(self.ac_bolos,self.tsshape)),self.datashape),
+      'PCA_astro':   lambda: reshape(efuncs(reshape(self.astrosignal,self.tsshape)),self.datashape),
+      'PCA_noise':   lambda: reshape(efuncs(reshape(self.noise,self.tsshape)),self.datashape),
+      'PCA_default': lambda: reshape(efuncs(reshape(self.atmo_one - self.atmosphere + self.astrosignal,self.tsshape)),self.datashape),
       }
 
       self.tsplot = 'default'
@@ -1280,7 +1287,7 @@ class Flagger:
           raise KeyError("Timestream %s is not valid." % timestream)
       self.plotfig=figure(fignum)
       if clear: self.plotfig.clear()
-      plot(wholedata[:,bolonum],linewidth=0.5, **kwargs)
+      plot(wholedata[:,bolonum],linewidth=0.5, label=str(bolonum), **kwargs)
   
   def write_ncdf(self):
       if not self.ncfile:
@@ -1448,7 +1455,7 @@ def _hdr_string_to_card(str):
 
 def _hdr_string_list_to_cardlist(strlist):
     cardlist = [_hdr_string_to_card(s) for s in strlist]
-    cardlist.remove(None)
+    while None in cardlist: cardlist.remove(None)
     return pyfits.CardList(cardlist)
 
 def itermedian(ts,scale=0.8,niter=5):

agpy/radialprofile.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-def azimuthalAverage(image, center=None, stddev=False, returnradii=False, return_nr=False ):
+def azimuthalAverage(image, center=None, stddev=False, returnradii=False, return_nr=False, 
+        binsize=0.5):
     """
     Calculate the azimuthally averaged radial profile.
 
@@ -11,6 +12,9 @@ def azimuthalAverage(image, center=None, stddev=False, returnradii=False, return
     stddev - if specified, return the azimuthal standard deviation instead of the average
     returnradii - if specified, return (radii_array,radial_profile)
     return_nr   - if specified, return number of pixels per radius *and* radius
+    binsize - size of the averaging bin.  Can lead to strange results if
+        non-binsize factors are used to specify the center and the binsize is
+        too large
     
     Taken from http://www.astrobetter.com/wiki/tiki-index.php?page=python_radial_profiles
     via Jessica R. Lu
@@ -29,39 +33,39 @@ def azimuthalAverage(image, center=None, stddev=False, returnradii=False, return
     i_sorted = image.flat[ind]
 
     # Get the integer part of the radii (bin size = 1)
-    r_int = r_sorted.astype(int)
+    r_binned = np.round(r_sorted/binsize) * binsize
 
     # Find all pixels that fall within each radial bin.
-    deltar = r_int[1:] - r_int[:-1]  # Assumes all radii represented
-    deltar[-1] = 1                   # include outermost points
+    deltar = r_binned[1:] - r_binned[:-1]  # Assumes all radii represented
+    deltar[-1] = binsize                   # include outermost points
     rind = np.where(deltar)[0] + 1   # location of changed radius (minimum 1 point)
-    nr = np.concatenate([rind[0:1], rind[1:] - rind[:-1]]) # number of radius bin
+    nr = np.concatenate([rind[0:1]+1, rind[1:] - rind[:-1]]) # number of radius bin
                                      # concatenate to include center pixel / innermost bin
 
     # Cumulative sum to figure out sums for each radius bin
     csim = np.cumsum(i_sorted, dtype=float)
-    tbin = np.concatenate([csim[0:1], csim[rind[1:]] - csim[rind[:-1]]]) # include innermost bin
+    tbin = np.concatenate([csim[rind[0]:rind[0]+1], csim[rind[1:]] - csim[rind[:-1]]]) # include innermost bin
 
     radial_prof = tbin / nr
 
     if stddev:
         # find azimuthal standard deviation
-        r_int[r_int==r_int.max()] = r_int.max() - 1  # set last bin equal to second-to-last bin
-        rad_mean = radial_prof[r_int]
+        r_binned[r_binned==r_binned.max()] = r_binned.max() - 1  # set last bin equal to second-to-last bin
+        rad_mean = radial_prof[r_binned]
         rad_diffsum = np.cumsum( (i_sorted-rad_mean)**2 )
         rad_std = np.sqrt( ( np.concatenate([rad_diffsum[0:1], rad_diffsum[rind[1:]] - rad_diffsum[rind[:-1]]]) ) / nr )
         
         if returnradii: 
-            return r_int[rind],rad_std
+            return r_binned[rind],rad_std
         elif return_nr:
-            return nr,r_int[rind],rad_std
+            return nr,r_binned[rind],rad_std
         else:
             return rad_std
 
     else:
         if returnradii: 
-            return r_int[rind],radial_prof
+            return r_binned[rind],radial_prof
         elif return_nr:
-            return nr,r_int[rind],radial_prof
+            return nr,r_binned[rind],radial_prof
         else:
             return radial_prof