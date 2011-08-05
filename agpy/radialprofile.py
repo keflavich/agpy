db36e5696e0aeca7efc9873fcccdc3fa3ff5bbd5
keflavich <keflavich@7a03b35a-38c0-11de-8fef-1f5ae417a2df>
2011-08-05 17:00:58 +0000

    A lot of time-profiling code for radial profiling.  I've found a solution that is ~20x faster.  All profiling code will be removed after this commit
    git-svn-id: https://agpy.googlecode.com/svn/trunk@345 7a03b35a-38c0-11de-8fef-1f5ae417a2df

--- agpy/radialprofile.py
+++ agpy/radialprofile.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def azimuthalAverage(image, center=None, stddev=False, returnradii=False, return_nr=False, 
         binsize=0.5, weights=None, steps=False, interpnan=False, left=None, right=None,
-        mask=None ):
+        mask=None, profile=False ):
     """
     Calculate the azimuthally averaged radial profile.
 
@@ -46,10 +46,9 @@ def azimuthalAverage(image, center=None, stddev=False, returnradii=False, return
         raise ValueError("Weighted standard deviation is not defined.")
 
     if mask is None:
-        # mask is only used in a flat context
-        mask = np.ones(image.shape,dtype='bool').ravel()
-    elif len(mask.shape) > 1:
-        mask = mask.ravel()
+        mask = np.ones(image.shape,dtype='bool')
+    # obsolete elif len(mask.shape) > 1:
+    # obsolete     mask = mask.ravel()
 
     # the 'bins' as initially defined are lower/upper bounds for each bin
     # so that values will be in [lower,upper)  
@@ -59,26 +58,78 @@ def azimuthalAverage(image, center=None, stddev=False, returnradii=False, return
     # but we're probably more interested in the bin centers than their left or right sides...
     bin_centers = (bins[1:]+bins[:-1])/2.0
 
-    # Find out which radial bin each point in the map belongs to
-    whichbin = np.digitize(r.flat,bins)
 
     # how many per bin (i.e., histogram)?
     # there are never any in bin 0, because the lowest index returned by digitize is 1
-    nr = np.bincount(whichbin)[1:]
+    #nr = np.bincount(whichbin)[1:]
+    nr = np.histogram(r,bins)[0]
 
     # recall that bins are from 1 to nbins (which is expressed in array terms by arange(nbins)+1 or xrange(1,nbins+1) )
     # radial_prof.shape = bin_centers.shape
     if stddev:
-        radial_prof = np.array([image.flat[mask*(whichbin==b)].std() for b in xrange(1,nbins+1)])
-    else: # this code is very slow.  The indexing causes the slowness.
-        # separate code for line-profiling radial_prof_a = np.zeros(nbins)
-        # separate code for line-profiling for b in xrange(1,nbins+1):
-        # separate code for line-profiling     flat_weighted_image = (image*weights).ravel()
-        # separate code for line-profiling     rad_sum = flat_weighted_image[mask*(whichbin==b)].sum()
-        # separate code for line-profiling     weight_sum = weights.flat[mask*(whichbin==b)].sum()
-        # separate code for line-profiling     radial_prof_a[b-1] = rad_sum/weight_sum
+        # Find out which radial bin each point in the map belongs to
+        whichbin = np.digitize(r.flat,bins)
+        radial_prof = np.array([image.flat[mask.flat*(whichbin==b)].std() for b in xrange(1,nbins+1)])
+    else: 
+        radial_prof = np.histogram(r, bins, weights=(image*weights*mask))[0] / np.histogram(r, bins, weights=(mask*weights))[0]
+        if profile:
 
-        radial_prof = np.array([(image*weights).flat[mask*(whichbin==b)].sum() / weights.flat[mask*(whichbin==b)].sum() for b in xrange(1,nbins+1)])
+            # Find out which radial bin each point in the map belongs to
+            whichbin = np.digitize(r.flat,bins)
+            # original, one line
+            radial_prof_orig = np.array([(image*weights).flat[mask.flat*(whichbin==b)].sum() / weights.flat[mask.flat*(whichbin==b)].sum() for b in xrange(1,nbins+1)])
+            # original
+            radial_prof_e = np.zeros(nbins)
+            for b in xrange(1,nbins+1):
+                flat_weighted_image = (image*weights).ravel()
+                rad_sum = flat_weighted_image[mask.flat*(whichbin==b)].sum()
+                weight_sum = weights.flat[mask.flat*(whichbin==b)].sum()
+                radial_prof_e[b-1] = rad_sum/weight_sum
+
+            # slowest
+            sortorder = np.argsort(whichbin)
+            radial_prof_a = np.zeros(nbins,dtype='float')
+            weights_a = np.zeros(nbins,dtype='float')
+            maskim = (image*mask).ravel()
+            for ind in sortorder:
+                radial_prof_a[whichbin[ind]-1] += maskim[ind]
+                weights_a[whichbin[ind]-1] += mask.flat[ind]
+            radial_prof_b = radial_prof_a/weights_a
+
+            # this is the only slow line, but it's twice as fast as the original
+            # yt-version inverse_bin_indices = dict( [(bin,np.nonzero(whichbin==bin)[0]) for bin in xrange(1,nbins+1)] )
+            inverse_bin_indices = [np.nonzero(whichbin==bin)[0] for bin in xrange(1,nbins+1)] 
+            inverse_bin_indices_b = np.histogram(whichbin,bins=range(1,nbins+1))
+            # the rest of this is reasonably fast...
+            radial_prof_c = np.zeros(nbins,dtype='float')
+            weights_c = np.zeros(nbins,dtype='float')
+            for binnum in xrange(nbins):
+                radial_prof_c[binnum] += maskim[inverse_bin_indices[binnum]].sum()
+                weights_c[binnum] += mask.flat[inverse_bin_indices[binnum]].sum()
+            radial_prof_d = radial_prof_c/weights_c
+
+            import pylab
+            print "orig == b?    ",sum(radial_prof_orig==radial_prof_b),"/",len(radial_prof)," chi^2: ",sum((radial_prof_orig-radial_prof_b)**2)
+            print "orig == d?    ",sum(radial_prof_orig==radial_prof_d),"/",len(radial_prof)," chi^2: ",sum((radial_prof_orig-radial_prof_d)**2)
+            print "orig == e?    ",sum(radial_prof_orig==radial_prof_e),"/",len(radial_prof)," chi^2: ",sum((radial_prof_orig-radial_prof_e)**2)
+            print "orig == hist? ",sum(radial_prof_orig==radial_prof  ),"/",len(radial_prof)," chi^2: ",sum((radial_prof_orig-radial_prof  )**2)
+            pylab.figure(1)
+            pylab.clf()
+            pylab.plot(radial_prof     -radial_prof,label='zero')
+            pylab.plot(radial_prof_orig-radial_prof,label='orig')
+            pylab.plot(radial_prof_b   -radial_prof,label='b')
+            pylab.plot(radial_prof_d   -radial_prof,label='d')
+            pylab.plot(radial_prof_e   -radial_prof,label='e')
+            leg=pylab.legend(loc='best')
+            leg.draggable()
+            pylab.draw()
+
+            if (sum((radial_prof-radial_prof_e)**2) > 1e-4 or
+                sum((radial_prof-radial_prof_d)**2) > 1e-4 or
+                sum((radial_prof-radial_prof_b)**2) > 1e-4):
+                raise ValueError("One of the methods failed.")
+
+        
 
     #import pdb; pdb.set_trace()
 
