8cfd7f8e439214041d61b56ae87622f35882db60
keflavich <keflavich@7a03b35a-38c0-11de-8fef-1f5ae417a2df>
2011-10-21 19:55:41 +0000

    added hot wind code
    git-svn-id: https://agpy.googlecode.com/svn/trunk@368 7a03b35a-38c0-11de-8fef-1f5ae417a2df

--- agpy/constants.py
+++ agpy/constants.py
@@ -5,17 +5,21 @@ A simple list of important constants in CGS units
 #constants
 hplanck = h = 6.626068e-27    # Planck's Constant (erg s)
 k = kb = kB = 1.3806503e-16   # Boltzmann's Constant (erg / K)
+G = 6.67300e-8                # Newton's constant (cm^3 g^-1 s^-2)
 c = 2.99792458e10             # Speed of Light (cm/s)
 m_co = 4.65e-23               # Mass of CO (g)
 m_h2 = 3.32e-24               # Mass of H_2 (g)
 msun = 1.9889e33              # grams
+lsun = 3.839e33               # erg/s
 h2toCO = 1e4                  # X_CO = CO abundance
 kmstocms  = 1e5               # conversion from km/s to cm/s
 au = 1.496e13                 # 1 astronomical unit in cm
 pc = 3.086e18                 # 1 parsec in cm
+muh=1.4                       # Mass conversion factor for H 
 muh2=2.8                      # Mass conversion factor for H2 (see Kauffman 2009)
 yr = year = 86400*365.        # 1 year in seconds
 amu = mh = 1.66053886e-24     # 1 atomic mass unit (1 hydrogen atom) in grams
+alphab = 2.6e-13              # Case B recombination coefficient for H
 pi = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679 # why not?
 #                                            ^
 # According to wikipedia, we only need accuracy to the 39th digit to make a
---
+++ agpy/hotwind.py
@@ -0,0 +1,52 @@
+# formulae for hot wind computations
+#
+# Panagia & Felli 1975 / Wright & Barlow 1975:
+from agpy.constants import *
+from numpy import log10,exp,log
+
+def mdot(snu, nu=10, Te=1e4, vinf=2000, mue=1.3, d=1):
+    """
+    [snu] = mJy
+    nu = 10 GHz (frequency)
+    Te = 10^4 K (ionized gas temp)
+    vinf = 2000 km/s (maximum wind speed)
+    mue = 1.3 (mean atomic weight per free electron)
+    d = distance (kpc)
+
+    return: mdot in Msun/yr
+    """
+    mdot = ( snu / 7.26 * (nu/10.)**-0.6 * (Te/1e4)**-0.1 * d**2 * (mue*vinf/100)**(4/3.) ) **(3/4.) * 1e-6
+    return mdot
+
+def mdotvinfr(snu, nu=10, Te=1e4, vinf=2000, mue=1.3, d=1, R=25):
+    """
+    [snu] = mJy
+    nu = 10 GHz (frequency)
+    Te = 10^4 K (ionized gas temp)
+    vinf = 2000 km/s (maximum wind speed)
+    mue = 1.3 (mean atomic weight per free electron)
+    d = distance (kpc)
+
+    return: Dmom in g*cm/s^2
+
+    Sp. type Log D0 x alphaP
+    A I 14.22 	 2.41 2.64 	 0.47 0.38 	 0.07
+    Mid B I 17.07 	 1.05 1.95 	 0.20 0.51 	 0.05
+    Early B I 21.24 	 1.38 1.34 	 0.25 0.75 	 0.15
+    O I 20.69 	 1.04 1.51 	 0.18 0.66 	 0.06
+    O III, V 19.87 	 1.21 1.57 	 0.21 0.64 	 0.06
+
+    """
+    mdot = ( snu / 7.26 * (nu/10.)**-0.6 * (Te/1e4)**-0.1 * d**2 * (mue*vinf/100)**(4/3.) ) **(3/4.) * 1e-6
+    return mdot*msun/yr * (vinf*1e5) * (R)**0.5
+
+def LofMdot(logdmom, x=1.51, D0=20.69, alphaP=0.66):
+    """
+    Kudritzki 2000
+    log Dmom - log D0 = x log(L/Lsun); Dmom = Mdot * vinf *R/rsun
+
+    returns L in log10(Lsun)
+    """
+
+    L = ( (logdmom-D0) / x )
+    return L
