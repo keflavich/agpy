5f82f4fe43ce7c7624ef466ec09e1a3eed2b8d27
keflavich <keflavich@7a03b35a-38c0-11de-8fef-1f5ae417a2df>
2011-11-18 05:35:26 +0000

    added Schechter CDF and generator
    git-svn-id: https://agpy.googlecode.com/svn/trunk@378 7a03b35a-38c0-11de-8fef-1f5ae417a2df

--- agpy/imf.py
+++ agpy/imf.py
@@ -28,10 +28,10 @@ def chabrier(m):
     not sure which of these to use...
     """
     # This system MF can be parameterized by the same type of lognormal form as
-    # the single MF (eq. [17]), with the same normalization at 1 M⊙, with the
+    # the single MF (eq. [17]), with the same normalization at 1 Msun, with the
     # coefficients (Chabrier 2003)
     return 0.86 * np.exp(-1*(log(m)-log(0.22))**2/(2*0.57**2))
-    # This analytic form for the disk MF for single objects below 1 M⊙, within these uncertainties, is given by the following lognormal form (Chabrier 2003):
+    # This analytic form for the disk MF for single objects below 1 Msun, within these uncertainties, is given by the following lognormal form (Chabrier 2003):
     return 0.158 * np.exp(-1*(log(m)-log(0.08))**2/(2*0.69**2))
 
 def schechter(m,A=1,beta=2,m0=100):
@@ -39,6 +39,40 @@ def schechter(m,A=1,beta=2,m0=100):
     A Schechter function with arbitrary defaults
     """
     return A*m**-beta * np.exp(-m/m0)
+
+try: 
+    import scipy
+    def schechter_cdf(m,A=1,beta=2,m0=100,mmin=10,mmax=None,npts=1e4):
+        """
+        Return the CDF value of a given mass for a set mmin,mmax
+        mmax will default to 10 m0 if not specified
+        
+        Analytic integral of the Schechter function:
+        http://www.wolframalpha.com/input/?i=integral%28x^-a+exp%28-x%2Fm%29+dx%29
+        """
+        if mmax is None:
+            mmax = 10*m0
+        
+        # integrate the CDF from the minimum to maximum 
+        # undefined posint = -m0 * mmax**-beta * (mmax/m0)**beta * scipy.special.gammainc(1-beta, mmax/m0)
+        # undefined negint = -m0 * mmin**-beta * (mmin/m0)**beta * scipy.special.gammainc(1-beta, mmin/m0)
+        posint = -mmax**(1-beta) * scipy.special.expn(beta, mmax/m0)
+        negint = -mmin**(1-beta) * scipy.special.expn(beta, mmin/m0)
+        tot = posint-negint
+
+        # normalize by the integral
+        # undefined ret = (-m0 * m**-beta * (m/m0)**beta * scipy.special.gammainc(1-beta, m/m0)) / tot
+        ret = (-m**(1-beta) * scipy.special.expn(beta, m/m0) - negint)/ tot
+
+        return ret
+
+    def sh_cdf_func(**kwargs):
+        return lambda x: schechter_cdf(x, **kwargs)
+except ImportError:
+    pass
+
+
+
 
 #def schechter_inv(m): 
 #    """
