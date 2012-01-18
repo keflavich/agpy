43651de5e285e1c25b98ed971f54b6fffe2ee027
keflavich <keflavich@7a03b35a-38c0-11de-8fef-1f5ae417a2df>
2012-01-18 20:10:33 +0000

    install script is more functional (not 100% sure it works still though)
    git-svn-id: https://agpy.googlecode.com/svn/trunk@423 7a03b35a-38c0-11de-8fef-1f5ae417a2df

--- macvim-skim/macvim-skim-install.sh
+++ macvim-skim/macvim-skim-install.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
 
 # set install path
-PREFIX="~/bin/"
-VIMDIR="~/.vim/"
+HOMEDIR=~
+PREFIX="$HOMEDIR/bin/"
+VIMDIR="$HOMEDIR/.vim/"
 
 # get relevant scripts
 echo "Getting macvim-load-line from googlecode repository"
 curl -s http://agpy.googlecode.com/svn/trunk/macvim-skim/macvim-load-line > ${PREFIX}/macvim-load-line
+chmod +x ${PREFIX}/macvim-load-line
 echo "Getting WhichTab.vim from googlecode repository"
-curl -s http://agpy.googlecode.com/svn/trunk/macvim-skim/WhichTab.vim > ${VIMDIR}/plugins/WhichTab.vim
+curl -s http://agpy.googlecode.com/svn/trunk/macvim-skim/WhichTab.vim > ${VIMDIR}/plugin/WhichTab.vim
 
-# get Skim.app (this has to be a hardlink; sourceforge doesn't seem to have a dynamic latest-version link)
-curl http://downloads.sourceforge.net/project/skim-app/Skim/Skim-1.3.19/Skim-1.3.19.dmg?r=http%3A%2F%2Fskim-app.sourceforge.net%2F&ts=1326914520&use_mirror=iweb > Skim-1.3.19.dmg
-hdid Skim-1.3.19.dmg
+# get Skim.app 
+# should work, but doesn't curl -L http://sourceforge.net/projects/skim-app/files/latest/download?source=files -o Skim.dmg
+curl -L "http://downloads.sourceforge.net/project/skim-app/Skim/Skim-1.3.19/Skim-1.3.19.dmg?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fskim-app%2F&ts=1326916683&use_mirror=superb-dca2" -o Skim.dmg
+hdid Skim.dmg
 cp -r /Volumes/Skim/Skim.app /Applications
+hdiutil eject /Volumes/Skim
 
 # Add lines to .vimrc 
 echo \" Activate skim >> ~/.vimrc
