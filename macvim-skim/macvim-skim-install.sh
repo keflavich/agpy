d779c1e7856c2ec4ef81bcd1e2b0d3ecc9cdbd7f
keflavich <keflavich@7a03b35a-38c0-11de-8fef-1f5ae417a2df>
2012-01-18 22:42:56 +0000

    updated install script to avoid re-installation
    git-svn-id: https://agpy.googlecode.com/svn/trunk@424 7a03b35a-38c0-11de-8fef-1f5ae417a2df

--- macvim-skim/macvim-skim-install.sh
+++ macvim-skim/macvim-skim-install.sh
@@ -12,21 +12,29 @@ chmod +x ${PREFIX}/macvim-load-line
 echo "Getting WhichTab.vim from googlecode repository"
 curl -s http://agpy.googlecode.com/svn/trunk/macvim-skim/WhichTab.vim > ${VIMDIR}/plugin/WhichTab.vim
 
-# get Skim.app 
-# should work, but doesn't curl -L http://sourceforge.net/projects/skim-app/files/latest/download?source=files -o Skim.dmg
-curl -L "http://downloads.sourceforge.net/project/skim-app/Skim/Skim-1.3.19/Skim-1.3.19.dmg?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fskim-app%2F&ts=1326916683&use_mirror=superb-dca2" -o Skim.dmg
-hdid Skim.dmg
-cp -r /Volumes/Skim/Skim.app /Applications
-hdiutil eject /Volumes/Skim
+if [ ! -d /Applications/Skim.app ]
+then
+    # get Skim.app 
+    # should work, but doesn't curl -L http://sourceforge.net/projects/skim-app/files/latest/download?source=files -o Skim.dmg
+    echo "Did not find /Applications/Skim.app.  Downloading and installing 1.3.19."
+    curl -L "http://downloads.sourceforge.net/project/skim-app/Skim/Skim-1.3.19/Skim-1.3.19.dmg?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fskim-app%2F&ts=1326916683&use_mirror=superb-dca2" -o Skim.dmg
+    hdid Skim.dmg
+    cp -r /Volumes/Skim/Skim.app /Applications
+    hdiutil eject /Volumes/Skim
+fi
 
 # Add lines to .vimrc 
-echo \" Activate skim >> ~/.vimrc
-echo 'map ,v :w<CR>:silent !/Applications/Skim.app/Contents/SharedSupport/displayline -r <C-r>=line(".")<CR> %<.pdf %<CR><CR>' >> ~/.vimrc
-echo 'map ,p :w<CR>:silent !pdflatex -synctex=1 --interaction=nonstopmode %:p <CR>:silent !/Applications/Skim.app/Contents/SharedSupport/displayline -r <C-r>=line(".")<CR> %<.pdf %<CR><CR>' >> ~/.vimrc
-echo 'map ,m :w<CR>:silent !make <CR>:silent !/Applications/Skim.app/Contents/SharedSupport/displayline -r <C-r>=line(".")<CR> %<.pdf %<CR><CR>' >> ~/.vimrc
-echo \" Reactivate VIM >> ~/.vimrc
-echo 'map ,r :w<CR>:silent !/Applications/Skim.app/Contents/SharedSupport/displayline -r <C-r>=line(".")<CR> %<.pdf %<CR>:silent !osascript -e "tell application \"MacVim\" to activate" <CR><CR>' >> ~/.vimrc
-echo 'map ,t :w<CR>:silent !pdflatex -synctex=1 --interaction=nonstopmode %:p <CR>:silent !/Applications/Skim.app/Contents/SharedSupport/displayline -r <C-r>=line(".")<CR> %<.pdf %<CR>:silent !osascript -e "tell application \"MacVim\" to activate" <CR><CR>' >> ~/.vimrc
+if [[ `grep 'map ,[rvpmt]' ${HOMEDIR}/.vimrc` == "" ]]; 
+then
+    echo "Did not find mappings in ${HOMEDIR}/.vimrc.  Appending them."
+    echo \" Activate skim >> ${HOMEDIR}/.vimrc
+    echo 'map ,v :w<CR>:silent !/Applications/Skim.app/Contents/SharedSupport/displayline -r <C-r>=line(".")<CR> %<.pdf %<CR><CR>' >> ${HOMEDIR}/.vimrc
+    echo 'map ,p :w<CR>:silent !pdflatex -synctex=1 --interaction=nonstopmode %:p <CR>:silent !/Applications/Skim.app/Contents/SharedSupport/displayline -r <C-r>=line(".")<CR> %<.pdf %<CR><CR>' >> ${HOMEDIR}/.vimrc
+    echo 'map ,m :w<CR>:silent !make <CR>:silent !/Applications/Skim.app/Contents/SharedSupport/displayline -r <C-r>=line(".")<CR> %<.pdf %<CR><CR>' >> ${HOMEDIR}/.vimrc
+    echo \" Reactivate VIM >> ${HOMEDIR}/.vimrc
+    echo 'map ,r :w<CR>:silent !/Applications/Skim.app/Contents/SharedSupport/displayline -r <C-r>=line(".")<CR> %<.pdf %<CR>:silent !osascript -e "tell application \"MacVim\" to activate" <CR><CR>' >> ${HOMEDIR}/.vimrc
+    echo 'map ,t :w<CR>:silent !pdflatex -synctex=1 --interaction=nonstopmode %:p <CR>:silent !/Applications/Skim.app/Contents/SharedSupport/displayline -r <C-r>=line(".")<CR> %<.pdf %<CR>:silent !osascript -e "tell application \"MacVim\" to activate" <CR><CR>' >> ${HOMEDIR}/.vimrc
+fi
 
 # set Skim settings
 defaults write net.sourceforge.skim-app.skim SKTeXEditorCommand "macvim-load-line"
